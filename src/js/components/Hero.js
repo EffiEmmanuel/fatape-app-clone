cc9b316e6f5234ae6f7ca903635c0f2c554d2fd3
Effi Emmanuel <effiemmanuel.n@gmail.com>
2022-03-23 10:48:12 +0100

    Disabled animations and changed the background image

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Countdown from "react-countdown";
 import Grid from "@mui/material/Grid";
-import background from "../assets/welcome-bg.png";
+import background from "../assets/bg.jpg";
 import heroApe from "../assets/apeDefault.png";
 import Eye from "../assets/Cinema Shades.png";
 import Head from "../assets/Afro With Comb.png";
@@ -16,105 +16,107 @@ import { SvgIcon } from '@mui/material';
 import addIcon from '../assets/addIcon.svg';
 
 // Renderer callback with condition
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  // Render a countdown
-  return (
-    <Grid
-      container
-      style={{
-        backgroundColor: "rgba(0,0,0,0.5)",
-        border: "solid 1px rgba(255,255,255,0.3)",
-        padding: ".875rem",
-      }}
-      className="countdown-grid"
-    >
-      <Grid item xs={3} className="countdown-item countdown-border-right">
-        <p className="countdown-value">{days}</p>
-        <p className="countdown-text">DAYS</p>
-      </Grid>
-      <Grid item xs={3} className="countdown-item countdown-border-right">
-        <p className="countdown-value">{hours}</p>
-        <p className="countdown-text">HOURS</p>
-      </Grid>
-      <Grid item xs={3} className="countdown-item countdown-border-right">
-        <p className="countdown-value">{minutes}</p>
-        <p className="countdown-text">MINUTES</p>
-      </Grid>
-      <Grid item xs={3} className="countdown-item">
-        <p className="countdown-value">{seconds}</p>
-        <p className="countdown-text">SECONDS</p>
-      </Grid>
-    </Grid>
+// const renderer = ({ days, hours, minutes, seconds, completed }) => {
+//   // Render a countdown
+//   return (
+//     <Grid
+//       container
+//       style={{
+//         backgroundColor: "rgba(0,0,0,0.5)",
+//         border: "solid 1px rgba(255,255,255,0.3)",
+//         padding: ".875rem",
+//       }}
+//       className="countdown-grid"
+//     >
+//       <Grid item xs={3} className="countdown-item countdown-border-right">
+//         <p className="countdown-value">{days}</p>
+//         <p className="countdown-text">DAYS</p>
+//       </Grid>
+//       <Grid item xs={3} className="countdown-item countdown-border-right">
+//         <p className="countdown-value">{hours}</p>
+//         <p className="countdown-text">HOURS</p>
+//       </Grid>
+//       <Grid item xs={3} className="countdown-item countdown-border-right">
+//         <p className="countdown-value">{minutes}</p>
+//         <p className="countdown-text">MINUTES</p>
+//       </Grid>
+//       <Grid item xs={3} className="countdown-item">
+//         <p className="countdown-value">{seconds}</p>
+//         <p className="countdown-text">SECONDS</p>
+//       </Grid>
+//     </Grid>
 
-  );
-};
+//   );
+// };
 
-const path_attr1 = {
-  curviness: 1,
-  autoRotate: false,
-  values: [
-    { x: 300, y: -300 },
-    { x: 400, y: -250 },
-  ],
-};
+// const path_attr1 = {
+//   curviness: 1,
+//   autoRotate: false,
+//   values: [
+//     { x: 300, y: -300 },
+//     { x: 400, y: -250 },
+//   ],
+// };
 
-const path_attr2 = {
-  curviness: 1,
-  autoRotate: false,
-  values: [
-    { x: -300, y: -300 },
-    { x: -400, y: 150 },
-  ],
-};
+// const path_attr2 = {
+//   curviness: 1,
+//   autoRotate: false,
+//   values: [
+//     { x: -300, y: -300 },
+//     { x: -400, y: 150 },
+//   ],
+// };
 
 const Hero = (props) => {
-  const [countdownCompleted, setCountdownCompleted] = useState(false);
+  // const [countdownCompleted, setCountdownCompleted] = useState(false);
 
-  useEffect(() => {
-    const tween = new TimelineMax();
+  // useEffect(() => {
+  //   const tween = new TimelineMax();
 
-    tween
-      .from(
-        ".heroAttr",
-        2,
-        {
-          bezier: path_attr1,
-          ease: Power1.easeInOut,
-        },
-        0
-      )
-      .from(
-        ".heroAttr2",
-        2,
-        {
-          bezier: path_attr2,
-          ease: Power1.easeInOut,
-        },
-        0
-      );
+  //   tween
+  //     .from(
+  //       ".heroAttr",
+  //       2,
+  //       {
+  //         bezier: path_attr1,
+  //         ease: Power1.easeInOut,
+  //       },
+  //       0
+  //     )
+  //     .from(
+  //       ".heroAttr2",
+  //       2,
+  //       {
+  //         bezier: path_attr2,
+  //         ease: Power1.easeInOut,
+  //       },
+  //       0
+  //     );
 
-    const controller = new ScrollMagic.Controller();
-    const scene = new ScrollMagic.Scene({
-      triggerElement: "#hero",
-      duration: 1250,
-      triggerHook: 0,
-    })
-      .setTween(tween)
-      // .addIndicators()
-      .setPin("#hero")
-      .addTo(controller);
-  }, []);
+  //   const controller = new ScrollMagic.Controller();
+  //   const scene = new ScrollMagic.Scene({
+  //     triggerElement: "#hero",
+  //     duration: 1250,
+  //     triggerHook: 0,
+  //   })
+  //     .setTween(tween)
+  //     // .addIndicators()
+  //     .setPin("#hero")
+  //     .addTo(controller);
+  // }, []);
 
-  if (countdownCompleted) {
-    console.log("countdownCompleted");
-  } else {
-    console.log(countdownCompleted);
-  }
+  // if (countdownCompleted) {
+  //   console.log("countdownCompleted");
+  // } else {
+  //   console.log(countdownCompleted);
+  // }
 
   return (
     <div
       style={{
         backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
       }}
     >
       <Container
@@ -154,7 +156,7 @@ const Hero = (props) => {
           <img src={heroApe} alt="fat ape" className="heroApe" />
         </div>
         <Box style={{ margin: "2em 0", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"space-between", width:"100%" }}>
-          <div className="countdown-container" style={{marginTop:"-3%"}}>
+          {/* <div className="countdown-container" style={{marginTop:"-3%"}}>
             <h3 style={{ fontSize: "30px" }}>Public Sale on November 1st, 9pm EST</h3>
             <Countdown
               date={Date.now() + 5000000}
@@ -165,11 +167,11 @@ const Hero = (props) => {
               
             />
 
-          </div>
+          </div> */}
 
           <div className="hero-minter">
 
-          <div class="incr-decr">
+          {/* <div class="incr-decr">
 
             <Button
               onClick={() =>
@@ -208,7 +210,7 @@ const Hero = (props) => {
               <span className="MuiTouchRipple-root"></span>
             </Button>
             
-            </div>
+            </div> */}
 
             <Button
               onClick={() =>
